675066238a34c6e67dda01ffdf66fed9d041b00b
Efaz Dipon <efazdipon@gmail.com>
2023-03-06 01:54:08 +0600

    iot washing machine sensor

--- routes/washingmachine.js
+++ routes/washingmachine.js
@@ -80,15 +80,6 @@ router.get('/:uid', async function(req, res, next) {
   }
 });
 
-router.delete('/', async function(req, res, next) {
-  try {
-    const wmachines = await WashingMachine.deleteMany();
-    res.status(200).json(wmachines);
-  } catch (error) {
-    res.status(422).json({ message: 'error', error: error.toString() })
-  }
-});
-
 router.get('/:uid/sensor', async function(req, res, next) {
   if (!req.params.uid) {
     return res.status(422).json({ message: 'error', errorMessage: 'Must provide a user id' });
@@ -101,24 +92,33 @@ router.get('/:uid/sensor', async function(req, res, next) {
       const sensors = await WashingMachineSensor.find({ washingMachineId: wm._id });
       allSensors.push(...sensors);
     }
-    res.status(200).json({ 
-      message: 'success', 
-      sensors: allSensors.map((sensor) => (
-        {
-          id: sensor._id,
-          washingMachineNickName: sensor.washingMachineNickName, 
-          sensorPurpose: sensor.sensorPurpose, 
-          sensorSectionName: sensor.sensorSectionName, 
-          sensorMacAddress: sensor.sensorMacAddress, 
-          isSwitchedOn: sensor.isSwitchedOn, 
-          washingMachineId: sensor.washingMachineId
-        }
-      )) 
-    });
+    res.status(200).json(
+      allSensors.map((sensor) => ({
+        id: sensor._id,
+        washingMachineNickName: sensor.washingMachineNickName, 
+        sensorPurpose: sensor.sensorPurpose, 
+        sensorSectionName: sensor.sensorSectionName, 
+        sensorMacAddress: sensor.sensorMacAddress, 
+        isSwitchedOn: sensor.isSwitchedOn, 
+        washingMachineId: sensor.washingMachineId
+      }))
+    );
   } catch (error) {
     res.status(422).json({ message: 'error', error: error.toString() });
   }
 });
+
+
+router.delete('/', async function(req, res, next) {
+  try {
+    const wmachines = await WashingMachine.deleteMany();
+    res.status(200).json(wmachines);
+  } catch (error) {
+    res.status(422).json({ message: 'error', error: error.toString() })
+  }
+});
+
+
 
 
 router.post('/sensor', async function(req, res, next) {
