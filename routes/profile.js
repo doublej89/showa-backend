4827729c47da5070eef86d3e7ad6cd149fe4bec8
Efaz Dipon <efazdipon@gmail.com>
2023-03-12 17:56:13 +0600

    profile 2

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 
 
 router.get('/:uid', async function(req, res, next) {
-    if (!req.params.id) {
+    if (!req.params.uid) {
       return res.status(422).json({ message: 'error', errorMessage: 'Must provide an user id' });
     }
     try {
@@ -21,37 +21,102 @@ router.get('/:uid', async function(req, res, next) {
 
 
 
-// router.post('/', async function(req, res, next) {
+router.post('/add-user-without-image', async function(req, res, next) {
   
-//     const { 
-//       uid, 
-//       shopCompanyName, 
-//       typeOfShop, 
-//       address, 
-//       typeOfWashingMachine, 
-//       brand, 
-//       model, 
-//       status 
-//     } = req.body;
-  
-  
-//     try {
-//       const wm = await WashingMachine.create({ 
-//         uid, 
-//         shopCompanyName, 
-//         typeOfShop, 
-//         address, 
-//         typeOfWashingMachine, 
-//         brand, 
-//         model, 
-//         status 
-//       });
-//       res.status(200).json({ message: 'success', washingMachine: wm });
-//     } catch (error) {
-//       res.status(422).json({ message: 'error', error: error.toString() })
-//     }
-//   });
+    const { 
+        uid,
+        email,
+        phone,
+        firstNameAlphabet,
+        middleNameAlphabet,
+        lastNameAlphabet,
+        firstNameKanji,
+        middleNameKanji,
+        lastNameKanji,
+        dob,
+        gender,
+        photoAddress,
+        postalCode,
+        prefecture,
+        cityAddress,
+        streetAddress,
+        buildingNameRoomNumber,
+        occupation
+    } = req.body;
   
   
+    try {
+      const user = await User.create({ 
+        uid,
+        email,
+        phone,
+        firstNameAlphabet,
+        middleNameAlphabet,
+        lastNameAlphabet,
+        firstNameKanji,
+        middleNameKanji,
+        lastNameKanji,
+        dob,
+        gender,
+        photoAddress,
+        postalCode,
+        prefecture,
+        cityAddress,
+        streetAddress,
+        buildingNameRoomNumber,
+        occupation
+      });
+      res.status(200).json({ message: 'success', user: user });
+    } catch (error) {
+      res.status(422).json({ message: 'error', error: error.toString() })
+    }
+  });
+  
+  
+  router.put('/user-address-info', async function(req, res, next) {
+    
+    const { 
+        uid,
+        email,
+        phone,
+        firstNameAlphabet,
+        middleNameAlphabet,
+        lastNameAlphabet,
+        firstNameKanji,
+        middleNameKanji,
+        lastNameKanji,
+        dob,
+        gender,
+        photoAddress,
+        postalCode,
+        prefecture,
+        cityAddress,
+        streetAddress,
+        buildingNameRoomNumber,
+        occupation
+    } = req.body;
+  
+  
+    try {
+      const user = await User.find({uid: uid});
+      if (!user) {
+        return res.status(422).json({ message: 'error', errorMessage: 'User with the given id do not exist' });
+      }
+      user.postalCode = postalCode;
+      user.prefecture = prefecture;
+      user.cityAddress = cityAddress;
+      user.streetAddress = streetAddress;
+      user.buildingNameRoomNumber = buildingNameRoomNumber;
+      user.occupation = occupation;
+    
+
+      const newuser = await user.save();
+      res.status(200).json({ message: 'success', user: newuser });
+    } catch (error) {
+      res.status(422).json({ message: 'error', error: error.toString() });
+    }
+  });
+  
+
 
 module.exports = router;
