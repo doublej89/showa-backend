4cd558490109fb44103ec232f6dbadc415c2f85a
Efaz Dipon <efazdipon@gmail.com>
2023-03-14 05:50:07 +0600

    get-my-showa-contact 11

--- routes/chat.js
+++ routes/chat.js
@@ -3,12 +3,12 @@ var router = express.Router();
 const User = require('../models/User');
 
 
-router.get('/get-my-showa-contact/:phones', async function(req, res, next) {
+router.get('/get-my-showa-contact', async function(req, res, next) {
     // if (!req.query.phones) {
     //   return res.status(422).json({ message: 'error', errorMessage: 'Must provide at least one phone number' });
     // }
     try {
-      const phones = req.params.phones.split(',');
+      const phones = req.query.phones.split(',');
       const usersByphone = [];
       for (const phone of phones) {
         const users = await User.find({ phone: { $regex: phone } });
