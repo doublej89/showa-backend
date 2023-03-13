1c3d20dab75e16c873f9b3607c55f6f7e06f3f37
Jawad Jahangir <jawadjahangir89@gmail.com>
2023-03-14 02:56:54 +0600

    users query by phone improved

--- routes/user_profile.js
+++ routes/user_profile.js
@@ -19,23 +19,23 @@ router.get('/:uid', async function(req, res, next) {
   });
 
 router.get('/', async function(req, res, next) {
-    if (!req.query.phones) {
-      return res.status(422).json({ message: 'error', errorMessage: 'Must provide at least one phone number' });
-    }
-    try {
-      const phones = req.query.phones.split(',');
-      const usersByphone = [];
-      for (const phone of phones) {
-        const users = await User.find({ phone });
-        if (users.length > 0) {
-          usersByphone.push(...users);
-        }
+  if (!req.query.phones) {
+    return res.status(422).json({ message: 'error', errorMessage: 'Must provide at least one phone number' });
+  }
+  try {
+    const phones = req.query.phones.split(',');
+    const usersByphone = [];
+    for (const phone of phones) {
+      const users = await User.find({ phone: { $regex: phone } });
+      if (users.length > 0) {
+        usersByphone.push(...users);
       }
-      res.status(200).json({ message: 'success', usersByphone });
-    } catch (error) {
-      res.status(422).json({ message: 'error', error: error.toString() })
     }
-  });
+    res.status(200).json({ message: 'success', usersByphone });
+  } catch (error) {
+    res.status(422).json({ message: 'error', error: error.toString() })
+  }
+});
 
 
 
