24f26ffcc6ac5a8be0375d6ec0022557f98d90fc
Efaz Dipon <efazdipon@gmail.com>
2023-03-14 17:06:13 +0600

    socket setup

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
+var http = require("http");
+const cors = require("cors");
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -12,6 +14,13 @@ var chatRouter = require('./routes/chat');
 const mongoose = require('mongoose');
 
 var app = express();
+var server = http.createServer(app);
+var io = require("socket.io")(server,{
+    cors: {
+        origin:"*"
+    }
+});
+
 
 
 const uri = "mongodb+srv://showadeveloper:yeY2831I52fakn0A@showadb.9ebpvry.mongodb.net/?retryWrites=true&w=majority";
@@ -33,6 +42,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/washing-machine', wmRouter);
@@ -44,6 +54,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+
 // error handler
 // app.use(function(err, req, res, next) {
 //   // set locals, only providing error in development
@@ -54,5 +65,11 @@ app.use(function(req, res, next) {
 //   res.status(err.status || 500);
 //   res.render('error');
 // });
+
+
+
+io.on("Connection", (socket) => {
+    console.log("connnected to socket server");
+});
 
 module.exports = app;
